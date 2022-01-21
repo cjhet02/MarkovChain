bec0b3a830d9833bce7bc8eb4c5e51a161107382
Jhet Cabigas <cjhet02@gmail.com>
2022-01-21 12:59:37 -0800

    added capability for different channel searches

--- TitleFetch.py
+++ TitleFetch.py
@@ -1,5 +1,5 @@
 from os import access
-import requests
+import requests as rq
 import json
 
 streamer = 'loltyler1'
@@ -9,18 +9,18 @@ Secret = '5pyubfbb49ndn91g4ktgv7972gf2tj'
 
 AuthParams = {'client_id' : Client_ID, 'client_secret' : Secret, 'grant_type' : 'client_credentials'}
 
-AuthCall = requests.post(url=authURL, params=AuthParams)
+AuthCall = rq.post(url=authURL, params=AuthParams)
 access_token = AuthCall.json()['access_token']
 
-#Streamer_ID = requests.get()
-URL = 'https://api.twitch.tv/helix/videos?user_id=51496027&first=100'
+Streamer_ID = rq.get('https://api.twitch.tv/helix/search/channels?query=' + streamer).json()['id']
+URL = 'https://api.twitch.tv/helix/videos?user_id=' + Streamer_ID + '&first=100'
 
 head = {
      'Client-ID' : Client_ID,
     'Authorization' : "Bearer " + access_token
 }
 
-r = requests.get(URL, headers = head).json()
+r = rq.get(URL, headers = head).json()
 
 for id in r['data']:
     print(id['title'])
