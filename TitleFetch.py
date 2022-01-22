383469618fc601f5ea81b4a26735b7e5f813a907
Jhet Cabigas <cjhet02@gmail.com>
2022-01-21 17:35:04 -0800

    finished
    added ability to change streamer, piped list of video titles into a free text generator api. It doesn't work very well.

--- TitleFetch.py
+++ TitleFetch.py
@@ -5,20 +5,31 @@ import json
 streamer = 'loltyler1'
 authURL = 'https://id.twitch.tv/oauth2/token'
 Client_ID = '6dzwm0ri8t01u98icpibehfw5boqpt'
-Secret = '5pyubfbb49ndn91g4ktgv7972gf2tj'
+Secret = '92u3zlx0pynkz92u4v5lq3z55lvcr2'
 
 AuthParams = {'client_id' : Client_ID, 'client_secret' : Secret, 'grant_type' : 'client_credentials'}
 
 AuthCall = rq.post(url=authURL, params=AuthParams)
 access_token = AuthCall.json()['access_token']
-
-Streamer_ID = rq.get('https://api.twitch.tv/helix/search/channels?query=' + streamer).json()['id']
-URL = 'https://api.twitch.tv/helix/videos?user_id=' + Streamer_ID + '&first=100'
-
 head = {
      'Client-ID' : Client_ID,
     'Authorization' : "Bearer " + access_token
 }
+channelURL = 'https://api.twitch.tv/helix/users?login=' + streamer
+channelSearch = rq.get(channelURL, headers = head)
+streamerID = channelSearch.json()['data'][0]['id']
 
-titles = rq.get(URL, headers = head).json()['title']
+vidURL = 'https://api.twitch.tv/helix/videos?user_id=' + streamerID + '&first=10'
+titles = rq.get(vidURL, headers = head).json()
 
+words = ""
+for video in titles['data']:
+    words += json.dumps(video['title'])
+
+r = rq.post("https://api.deepai.org/api/text-generator",
+    data={
+        'text': words,
+    },
+    headers={'api-key': 'quickstart-QUdJIGlzIGNvbWluZy4uLi4K'}
+)
+print(r.json())
